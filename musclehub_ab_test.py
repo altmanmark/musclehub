d5238c57eeecd2862d758f687adae80c6d437fd7
Mark <mark@Marks-MacBook-Pro-2.local>
2018-06-01 07:07:20 -0400

    started ab test group addition

--- musclehub_ab_test.py
+++ musclehub_ab_test.py
@@ -5,7 +5,8 @@ Created on Thu May 31 07:03:42 2018
 
 @author: mark
 """
-
+#Import SQLite database from Codecademy and examine the tables
+#Tables included are visits, fitness_tests, applications, purchases
 from codecademySQL import sql_query
 sql_query('''
 SELECT *
@@ -31,6 +32,9 @@ FROM purchases
 LIMIT 5
 ''')
 
+#Create giant database from the 4 provided datasets
+#Createa  query where we pull data from visits on or after 7-1-17.
+#Left join the other tabels based on first_name, last_name, and email
 df = sql_query('''
 SELECT visits.first_name,
        visits.last_name,
@@ -53,3 +57,14 @@ LEFT JOIN purchases
     AND purchases.email = visits.email
 WHERE visits.visit_date >= '7-1-17'
 ''')
+
+
+#Investigate groups A and B
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Add column to new dataset
+#A if fitness_test_date is not None and B if fitness_test_date is None
+df['ab_test_group'] = df.fitness_test_date.apply(lambda x: \
+  'B' if x == 'None' else 'A')
+print(df.head(10))
